1845b9b4a08dca798e9b3ca7f8e764536dbb135b
fasjesus <developerflavia@gmail.com>
2024-06-01 06:23:45 -0300

    Comentários

--- HotelHUB/db/reserva_hotel.sql
+++ HotelHUB/db/reserva_hotel.sql
@@ -1,3 +1,7 @@
+-- Esse Database foi criado por Brenda C. da Silva e Flávia A. S. de Jesus para a composição de um crédito na 
+-- disciplina de Banco de Dados.           
+-- Maio/Junho de 2024.
+
 CREATE DATABASE IF NOT EXISTS reserva_hotel; 
 
 USE reserva_hotel;
@@ -30,9 +34,9 @@ CREATE TABLE RESERVA (
     FOREIGN KEY (FK_cpf) REFERENCES CLIENTE(cpf) ON DELETE CASCADE
 );
 
--- INSERÇÕES
+-- INSERÇÕES:
 
--- Insere quartos
+-- Insere quartos:
 INSERT INTO QUARTO (NumeroQuarto, TipoQuarto, Preco, Status)
 VALUES 
     ('101', 'Standard', 150.00, 'Disponível'),
@@ -46,7 +50,7 @@ VALUES
     ('203', 'Luxo', 200.00, 'Disponível');
 
 
--- Insere clientes
+-- Insere clientes:
 INSERT INTO CLIENTE (user_name, nome_completo, cpf, idade, senha, email) 
 VALUES ("fullflavy", "Flávia Alessandra Santos de Jesus", "12134356578", 21, "fravineas123", "fullflavy@gmail.com");
 
@@ -57,7 +61,7 @@ INSERT INTO CLIENTE (user_name, nome_completo, cpf, idade, senha, email)
 VALUES ("bcastro", "Brenda Castro", "12223334456", 21, "brenda123", "brenda@gmail.com");
 
 
--- Insere reservas
+-- Insere reservas:
 INSERT INTO RESERVA (FK_Id_quarto, FK_cpf, DataCheckIn, DataCheckOut)
 VALUES (1, "12134356578", "2024-05-24", "2024-06-01");
 
@@ -69,35 +73,40 @@ VALUES (3, "12223334456", "2024-05-25", "2024-06-02");
 
 
 -- Atualiza o status do quarto (pois quando um usuario faz a reserva do quarto, 
--- a reserva dele precisa ser atualizada para Status = ocupado)
+-- a reserva dele precisa ser atualizada para Status = ocupado):
 UPDATE QUARTO
 SET Status = "Ocupado"
 WHERE Id_quarto = 1;
 
 -- Atualiza o status do quarto (pois quando um usuario faz a reserva do quarto, 
--- a reserva dele precisa ser atualizada para Status = ocupado
+-- a reserva dele precisa ser atualizada para Status = ocupado:
 UPDATE QUARTO 
 SET Status = "Ocupado"
 WHERE Id_quarto = 2;
 
 -- Atualiza o status do quarto (pois quando um usuario faz a reserva do quarto, 
--- a reserva dele precisa ser atualizada para Status = ocupado
+-- a reserva dele precisa ser atualizada para Status = ocupado:
 UPDATE QUARTO
 SET Status = "Ocupado"
 WHERE Id_quarto = 3;
 
--- Mostra todos os quartos reservados
+-- Mostra todos os quartos reservados:
 SELECT NumeroQuarto, TipoQuarto FROM QUARTO
 JOIN RESERVA ON QUARTO.Id_quarto = RESERVA.FK_Id_quarto
 JOIN CLIENTE ON CLIENTE.cpf = RESERVA.FK_cpf; 
 
--- Mostra os quartos disponíveis (vai mostrar 6 quartos)
+-- Mostra os quartos disponíveis (vai mostrar 6 quartos):
 SELECT NumeroQuarto, TipoQuarto, Preco, QUARTO.Status FROM QUARTO
 WHERE QUARTO.Status = "Disponível";
 
--- Deleta o quarto na posição 9 (última posição)
+-- Deleta o quarto na posição 9 (última posição):
 DELETE FROM QUARTO WHERE Id_quarto = '9';
 
--- Mostra os quartos disponíveis (vai mostrar 5 quartos)
+-- Mostra os quartos disponíveis (vai mostrar 5 quartos):
 SELECT NumeroQuarto, TipoQuarto, Preco, QUARTO.Status FROM QUARTO
 WHERE QUARTO.Status = "Disponível";
+
+-- Mostra as tabelas:
+-- SELECT * FROM CLIENTE;
+-- SELECT * FROM QUARTO;
+-- SELECT * FROM RESERVA;
